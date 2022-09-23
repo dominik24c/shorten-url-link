19bbc45f720b6554cd411e2aa12f87c61279833f
dominik24c <dominik24c@gmail.com>
2022-09-24 00:04:57 +0200

    add index for alias url field and remove for origin url

--- url_generator/models.py
+++ url_generator/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class GeneratedUrls(models.Model):
-    origin_url = models.URLField(unique=True, db_index=True)
-    alias_url = models.CharField(max_length=60, unique=True)
+    origin_url = models.URLField(unique=True)
+    alias_url = models.CharField(max_length=60, db_index=True, unique=True)
     ip_user = models.CharField(max_length=30)
     visited = models.IntegerField(default=0)
 
